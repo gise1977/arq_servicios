15f6a2ef957f54b923526ca53e0d1c47493955b3
lacc2704 <lacc2704@gmail.com>
2023-06-21 19:28:39 -0300

    daleee

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = express();
 app.listen(3000);
 console.log("server funcionando en el puerto", 3000);
 
-app.get ("/alumno",(req,res) => {
-    res.send ("id","nombre y apellido","notas","nota final","condicion")
+app.get ("/alumnos",(req,res) => {
+    res.send ("ponete las pilas giselaaa")
 });
 
 app.post ("/alumno/:id",(req,res) => {

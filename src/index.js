237ef267ade14b3b8a23ebf8179da7e474f5d035
lacc2704 <lacc2704@gmail.com>
2023-06-21 20:04:39 -0300

    sisi

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import mysql from "mysql";
+const base = mysql.createConnection({
+    host :"127.0.0.1",
+    user : "root",
+    database: "bda_alumnos"
+})
+
 const app = express();
 app.listen(3000);
 console.log("server funcionando en el puerto", 3000);
 
 app.get ("/alumnos",(req,res) => {
-    res.send ("ponete las pilas giselaaa")
+    base.query("SELECT * FROM bda_alumnos.notas", function (error,result){
+        res.send(result)
+    })
 });
 
 app.post ("/alumno/:id",(req,res) => {
